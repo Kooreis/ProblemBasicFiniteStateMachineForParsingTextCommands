f160421dceb72fed713ff264b9d05f713617dd5e
Kooreis <kooreis.official@gmail.com>
2024-08-02 21:14:46 -0500

    This is the initial implementation of the StateMachine class. It includes a constructor that initializes an empty states object and a null currentState. It also includes an addState method for adding new states to the states object and setting the currentState if it's not already set, and a transition method for changing the currentState.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple implementation of a finite state machine for parsing text commands in JavaScript:
-
-```javascript
 class StateMachine {
     constructor() {
         this.states = {};
@@ -15,46 +12,4 @@ class StateMachine {
     transition(newState) {
         this.currentState = newState;
     }
-
-    execute(command) {
-        const state = this.states[this.currentState];
-        if (!state) throw new Error(`State '${this.currentState}' does not exist`);
-        state.execute(command, this);
-    }
 }
-
-class State {
-    execute(command, stateMachine) {
-        throw new Error('execute method must be overridden');
-    }
-}
-
-class CommandState extends State {
-    execute(command, stateMachine) {
-        console.log(`Executing command '${command}' in state '${stateMachine.currentState}'`);
-        if (command === 'NEXT') stateMachine.transition('nextState');
-    }
-}
-
-class NextState extends State {
-    execute(command, stateMachine) {
-        console.log(`Executing command '${command}' in state '${stateMachine.currentState}'`);
-        if (command === 'PREV') stateMachine.transition('commandState');
-    }
-}
-
-const stateMachine = new StateMachine();
-stateMachine.addState('commandState', new CommandState());
-stateMachine.addState('nextState', new NextState());
-
-stateMachine.execute('NEXT');
-stateMachine.execute('PREV');
-```
-
-This code defines a `StateMachine` class that maintains a dictionary of states and the current state. The `addState` method is used to add new states to the machine, and the `transition` method is used to transition between states. The `execute` method is used to execute a command in the current state.
-
-The `State` class is a base class for all states. It defines an `execute` method that throws an error if it is not overridden in a derived class.
-
-The `CommandState` and `NextState` classes are derived from the `State` class and override the `execute` method to execute commands and transition to other states.
-
-The last part of the code creates a state machine, adds the `CommandState` and `NextState` states to it, and executes some commands.
